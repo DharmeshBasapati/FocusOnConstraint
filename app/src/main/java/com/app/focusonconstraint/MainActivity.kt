5cbdcbc76f1df2796fe8c68cbc1f6713594b29c3
Dharmesh Basapati <dharmesh.basapati@spacestem.com>
2021-11-08 14:06:36 +0530

    Initial commit

---
+++ app/src/main/java/com/app/focusonconstraint/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.app.focusonconstraint
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_for_chains)
+    }
+
+}
