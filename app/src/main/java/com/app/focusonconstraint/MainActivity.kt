eaa8241b43675ca850649c9e1eda23c18a7bfea9
Dharmesh Basapati <dharmesh.basapati@spacestem.com>
2021-11-08 17:00:27 +0530

    Added other features.

--- app/src/main/java/com/app/focusonconstraint/MainActivity.kt
+++ app/src/main/java/com/app/focusonconstraint/MainActivity.kt
@@ -1,13 +1,32 @@
 package com.app.focusonconstraint
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.Group
 
 class MainActivity : AppCompatActivity() {
 
+    private var group: Group? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_for_chains)
+        setContentView(R.layout.activity_for_groups)
+        group = findViewById(R.id.group)
+    }
+
+    fun manageGroupVisibility(view: View) {
+        when (view.id) {
+            R.id.btnVisible -> {
+                group?.visibility = View.VISIBLE
+            }
+            R.id.btnInvisible -> {
+                group?.visibility = View.INVISIBLE
+            }
+            R.id.btnGone -> {
+                group?.visibility = View.GONE
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/app/focusonconstraint/PlayStoreReplicaActivity.kt
@@ -0,0 +1,11 @@
+package com.app.focusonconstraint
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlayStoreReplicaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_store_replica)
+    }
+}
